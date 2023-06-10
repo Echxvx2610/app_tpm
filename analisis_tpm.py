69031d3e2c05772555c93fc97ac7b35152b83861
Echxvx2610 <cristianecheverriamendoza@gmail.com>
2023-06-10 08:46:16 +0000

    Test gitpod

--- analisis_tpm.py
+++ analisis_tpm.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-linea_1 = pandas.read_csv(r'LINEA 1\Datos Operadores.csv')
-df = pandas.DataFrame(linea_1)
-print(df)
+linea_1 = pd.read_csv(r'LINEA 1/Datos Operadores.csv')
+df = pd.DataFrame(linea_1)
+print(df,'\n')
+print(df["Nombre"],'\n')
